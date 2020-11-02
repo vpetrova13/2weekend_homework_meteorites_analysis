30bcaf392b5443bb0146895962d77067f7f03dd4
vpetrova13 <vpetrova1364@gmail.com>
2020-11-02 11:02:21 +0000

    uodate after revision

--- code/clean_meteorites_data.R
+++ code/clean_meteorites_data.R
@@ -4,11 +4,14 @@ library(janitor)
 library(assertr)
 
 #Read data and explore
-meteorites <- read_csv("meteorite_landings.csv") %>%  clean_names()
+meteorites <- read_csv("data/meteorite_landings.csv") %>%  clean_names()
 head(meteorites)
 names(meteorites)
 dim(meteorites)
 
+stopifnot(
+  names(meteorites) == c("id", "name", "mass_g", "fall", "year", "geo_location")
+)
 #Change names of variables to follow naming standards
 meteorites <- meteorites %>% 
   mutate(name = (str_to_lower(name))) %>% 
@@ -39,10 +42,38 @@ meteorites$latitude <- as.numeric(as.character(meteorites$latitude),  length = 1
 meteorites$longitude <- as.numeric(as.character(meteorites$longitude), length = 10)
 
 #Assertive programming
-meteorites %>% 
-  #verify(cols(1:6) == cols("id", "name", "mass (g)", "fall", "year", "GeoLocation")) %>% 
+meteorites %>%
   verify(latitude > -90 & latitude < 90) %>% 
   verify(longitude > -180 & longitude < 180)
 
 
 meteorites
+
+write_csv(meteorites, "data/clean_meteorites.csv")
+
+
+
+
+
+
+meteorites <-
+  meteorites %>% 
+  rename(
+    mass = "mass (g)",
+    lat_lng = "GeoLocation"
+  ) %>% 
+  mutate(
+    lat_lng = str_remove(lat_lng, fixed("(")),
+    lat_lng = str_remove(lat_lng, fixed(")"))
+  ) %>% 
+  separate(lat_lng, c("lat", "lng"), sep = ", ") %>% 
+  mutate(
+    lat = as.numeric(lat),
+    lng = as.numeric(lng),
+    lat = coalesce(lat, 0),
+    lng = coalesce(lng, 0)
+  ) %>% 
+  filter(
+    mass >= 1000
+  ) %>% 
+  arrange(year)
